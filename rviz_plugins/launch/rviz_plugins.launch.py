1e4e646b7d855138bc3a9753cf61be7fc1472d4f
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-06-16 13:53:48 +0000

    add default launch file

---
+++ rviz_plugins/launch/rviz_plugins.launch.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, SetParameter
+
+
+def generate_launch_description():
+    """Generate the launch description for the rviz_plugins node."""
+
+    remappable_topics = []
+
+    args = [
+        DeclareLaunchArgument("name", default_value="rviz", description="node name"),
+        DeclareLaunchArgument("namespace", default_value="", description="node namespace"),
+        DeclareLaunchArgument(
+            "config",
+            default_value=os.path.join(get_package_share_directory("rviz_plugins"), "config", "conf.rviz"),
+            description="path to rviz config file",
+        ),
+        DeclareLaunchArgument(
+            "log_level", default_value="info", description="ROS logging level (debug, info, warn, error, fatal)"
+        ),
+        DeclareLaunchArgument("use_sim_time", default_value="false", description="use simulation clock"),
+        *remappable_topics,
+    ]
+
+    nodes = [
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            namespace=LaunchConfiguration("namespace"),
+            name=LaunchConfiguration("name"),
+            arguments=["-d", LaunchConfiguration("config"), "--ros-args", "--log-level", LaunchConfiguration("log_level")],
+            remappings=[(la.default_value[0].text, LaunchConfiguration(la.name)) for la in remappable_topics],
+            output="screen",
+            emulate_tty=True,
+        )
+    ]
+
+    return LaunchDescription(
+        [
+            *args,
+            SetParameter("use_sim_time", LaunchConfiguration("use_sim_time")),
+            *nodes,
+        ]
+    )
